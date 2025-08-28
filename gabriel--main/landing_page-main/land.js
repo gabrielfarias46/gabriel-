aa48dafc01826abad70af0fa490c941e5f4ad61a
gabrielfarias46 <frigo.farias@escola.pr.gov.br>
2025-08-28 11:17:19 -0300

    Add files via upload

---
+++ gabriel--main/landing_page-main/land.js
@@ -0,0 +1,28 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const formData = new FormData(event.target);
+    const data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+
+    console.log(data); // Adiciona esta linha para verificar os dados no console
+
+    fetch('https://script.google.com/macros/s/AKfycbwiHbYAx3-Ba01VViKIhgv0FVrlqZ3PXBRRstoNNdpualDfEWZejT-dQDdcU3v2zB7h_w/exec', {
+        method: 'POST',
+        body: new URLSearchParams(data)
+    })
+    .then(response => response.json())
+    .then(responseData => {
+        if (responseData.result === 'success') {
+            alert('Dados enviados com sucesso!');
+            event.target.reset(); // Limpa o formulário
+        } else if (responseData.result === 'error' && responseData.message === 'Email already exists') {
+            alert('Erro: O email já existe.');
+        } else {
+            alert('Erro ao enviar os dados.');
+        }
+    })
+    .catch(error => console.error('Error:', error));
+});
